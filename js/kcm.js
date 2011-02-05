625bd3fe1f8918352dcf7692a048e6db6912ab94
igkuk7 <igkuk7@gmail.com>
2011-02-05 19:44:13 +0000

    Tidying up JS code. Tweaking HTML to point to correct js files. Added handling of some basic selection of cells in the grid.

--- js/kcm.js
+++ js/kcm.js
@@ -2,8 +2,8 @@ var kcm = window.kcm || {};
 
 kcm.main_grid = null;
 window.onload = function() {
-	kcm.main_grid = new kcm.KnittingChartMaker(
-		"kcm_canvas",
-		"kcm_chart_options"
-	);
+    kcm.main_grid = new kcm.KnittingChartMaker(
+        "kcm_canvas",
+        "kcm_chart_options"
+    );
 };
