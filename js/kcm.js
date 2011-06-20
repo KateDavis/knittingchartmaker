efa763779eab00bca9f14a055f422c795bd35bcf
igkuk7 <igkuk7@gmail.com>
2011-06-20 20:07:00 +0000

    Issue 23: selecting stitches, refactoring, nice colour chooser

--- js/kcm.js
+++ js/kcm.js
@@ -4,6 +4,16 @@ kcm.main_grid = null;
 window.onload = function() {
     kcm.main_grid = new kcm.KnittingChartMaker(
         "kcm_canvas",
-        "kcm_chart_options"
+        "kcm_chart_width",
+        "kcm_chart_height",
+        "kcm_chart_resize",
+        "kcm_chart_stitch",
+        "kcm_chart_stitch_size",
+        "kcm_chart_yarn",
+        "kcm_chart_yarn_colour",
+        "kcm_chart_save",
+        "kcm_chart_load",
+        "kcm_chart_img_png",
+        "kcm_chart_img_svg"
     );
 };
